279c8468bcc0578465141afa2489dc6388178d31
IbrahimGanaMalgwi <108299271+IbrahimMalgwi@users.noreply.github.com>
2022-10-16 21:01:20 +0100

    update to code

---
+++ algorithm_and_data_structure/linear_search.py
@@ -0,0 +1,25 @@
+def linear_search(lst, target):
+    """
+    returns the index position of the target if found, else return none
+    """
+
+    for i in range(0, len(lst)):
+        if lst[i] == target:
+            return i
+    return None
+
+
+def verify(index):
+    if index is not None:
+        print("index found at index", index)
+    else:
+        print("target not found in lst")
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = linear_search(numbers, 12)
+verify(result)
+
+result = linear_search(numbers, 6)
+verify(result)
